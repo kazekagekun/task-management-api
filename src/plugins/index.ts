9a54260902848ef11d377a2d84db462bb3d693e2
Hafiz <kazekagekun94@gmail.com>
2024-10-10 18:34:14 +0800

    Add prisma, prisma schema and enhance plugins

---
+++ src/plugins/index.ts
@@ -0,0 +1,13 @@
+import { FastifyInstance } from 'fastify';
+
+import fastifyPlugin from 'fastify-plugin';
+import config from './config';
+import prisma from './prisma';
+
+export default fastifyPlugin(async (fastify: FastifyInstance) => {
+	await Promise.all([fastify.register(config)]);
+
+	await Promise.all([
+		fastify.register(prisma),
+	]);
+});
