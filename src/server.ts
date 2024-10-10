7c9d863ad005d6ba702b8c998eed962bc0acbab7
Hafiz <kazekagekun94@gmail.com>
2024-10-10 17:55:58 +0800

    Update husky pre-commit config and eslint

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,21 @@ import dotenv from 'dotenv';
 import { fastifyInitialize } from '.';
 
 const start = async () => {
-    dotenv.config();
-    let fastify;
+	dotenv.config();
+	let fastify;
 
-    try {
-        fastify = await fastifyInitialize();
-    } catch (error) {
+	try {
+		fastify = await fastifyInitialize();
+	} catch (error) {
 		console.error('error initializing fastify', error);
 		console.error(error);
 		return;
-    }
+	}
 
-    await fastify.listen({
-		host: "127.0.0.1",
+	await fastify.listen({
+		host: '127.0.0.1',
 		port: 3000,
 	});
-}
+};
 
 start();
