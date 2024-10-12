2464eadcd8b295cd236d0ad4430b59fec37fd69d
Hafiz <kazekagekun94@gmail.com>
2024-10-12 17:36:15 +0800

    Update cors and paginations

--- src/plugins/config.ts
+++ src/plugins/config.ts
@@ -54,7 +54,8 @@ export default fastifyPlugin(
 				ALLOWED_ORIGINS: {
 					type: 'string',
 					separator: ',',
-					default: 'http://localhost:3000, http://0.0.0.0:3000, http://127.0.0.1:3000',
+					default:
+						'http://localhost:3000,http://0.0.0.0:3000,http://127.0.0.1:3000,http://localhost:5173',
 				},
 			},
 		};
